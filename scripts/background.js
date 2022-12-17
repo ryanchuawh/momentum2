c8446b6f88d15f5e994efd0a05833a445430e235
ryanchuawh <110446373+ryanchuawh@users.noreply.github.com>
2022-12-17 11:03:55 +0800

    improved to cleaner code

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,6 @@
-var backImg = ["assets/img/place1.jpg", "assets/img/place2.jpg", "assets/img/place3.jpg", "assets/img/place4.jpg", "assets/img/place5.jpg"];
-var today = new Date();
-var day = Math.floor(today/8.64e7);
+let backImg = ["assets/img/place1.jpg", "assets/img/place2.jpg", "assets/img/place3.jpg", "assets/img/place4.jpg", "assets/img/place5.jpg"];
+let today = new Date();
+let day = Math.floor(today/8.64e7);
 document.getElementById('main').style.backgroundImage = 'url('+backImg[day%5]+')';
+
+let quotes = ["I guess it simply goes to show that stuff will come and stuff will go. But do we cry? Goodness, NO! We keep on singing. <br> -Pete the Cat", ]
--- scripts/settingsmodal.js
+++ scripts/settingsmodal.js
@@ -1,6 +1,6 @@
-var modal = document.getElementById('settingsmodal')
-var modalBtn = document.getElementById('settings');
-var closeBtn = document.getElementById('closeBtn');
+let modal = document.getElementById('settingsmodal')
+let modalBtn = document.getElementById('settings');
+let closeBtn = document.getElementById('closeBtn');
 
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
